915a0af12dd3c5ee11c90a93040b60c52c4d8959
yhstart <twitter647804909@gmail.com>
2020-05-21 15:19:48 +0800

    Initial commit

---
+++ Module01/src/com/yuanhuan/bean/Customer.java
@@ -0,0 +1,45 @@
+package com.yuanhuan.bean;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+import sun.security.provider.PolicySpiFile;
+
+import javax.xml.bind.PrintConversionEvent;
+
+public class Customer {
+    public static void main(String[] args) {
+
+PrintConversionEventp
+        printConversionEventp
+        /**
+         *
+         */
+         private int  ;
+         /**
+          *
+          */
+          private String ;
+          
+
+/**
+ * 
+ */
+ private int  = ;
+ 
+
+        printConversionEventp
+        PrintConversionEvent
+    }
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static final int  = ;
+    public void test(){
+        PrintConversionEvent
+    }
+
+    }
+  }
